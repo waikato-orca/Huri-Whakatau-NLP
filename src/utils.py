d1d7a4ee2d11c050dc813e4f6eca5753457d78a2
pjv6 <priyankvyas1998@gmail.com>
2021-03-01 15:28:27 +1300

    Added functions to read the data from the database and display it on the window

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,21 @@
+from tkinter import *
+from tkinter import filedialog
+from sqlreader import sqlReader
+
 #Opens the file with the raw data that is to be analysed
-def openFile():
-    return
+def openFile(window):
+    file = filedialog.askopenfilename(initialdir="/Users/Admin/Desktop/Huri-Whakatau-NLP/data/raw", title="Select Discussion", filetypes=(("json files", "*.json"),("all files", "*.*")))
+    filename = file.split('/')[-1]
+    window.dirname = file.split('/')[-2]
+    window.reader = sqlReader("root", "123456789", "jr_slack")
+    sql = "(SELECT filename, dirname, text, user, ts FROM t_message WHERE subtype IS NULL and dirname = \"" + window.dirname + "\" and filename = \"" + filename + "\" UNION SELECT filename, dirname, topic, user, ts FROM t_message WHERE subtype = \"channel_topic\" AND dirname = \"" + window.dirname + "\" and filename = \"" + filename + "\" UNION SELECT filename, dirname, purpose, user, ts FROM t_message WHERE subtype = \"channel_purpose\" AND dirname = \"" + window.dirname + "\" and filename = \"" + filename + "\") ORDER BY ts"
+    data = window.reader.read_data(sql)
+    window.sentences, window.users = window.reader.sentenceExtraction(data)
+    window.discussion_listbox.delete(0, END)
+    showDiscussion(window)
+
+#Displays the discussion in the given format to view the raw data
+def showDiscussion(window):
+    for sentence, user in zip(window.sentences, window.users):
+            window.discussion_listbox.insert(END, user + ": " + sentence)
+            # self.discussion_listbox2.insert(END, user + ": " + sentence)
--- src/window.py
+++ src/window.py
@@ -1,12 +1,15 @@
-from tkinter import ttk, Menu
+from tkinter import *
+from tkinter import ttk
 from utils import openFile
 
-##Window class that populates the root window and handles all the tabs
+##Window class that populates the root window and handles all the tabs and widgets
 class Window:
     def __init__(self, root):
         self.root = root
         self.createMenu()
         self.createTabs()
+        self.createFrames()
+        self.createListboxes()
 
     #Create the menu bar and add the required options
     def createMenu(self):
@@ -14,26 +17,44 @@ class Window:
         self.root.config(menu = rootMenu)
         fileMenu = Menu(rootMenu)
         rootMenu.add_cascade(label = "File", menu = fileMenu)
-        fileMenu.add_command(label = "Open Discussion...", command = openFile)
+        fileMenu.add_command(label = "Open Discussion...", command = lambda: openFile(self))
         modelMenu = Menu(rootMenu)
         rootMenu.add_cascade(label = "Model", menu = modelMenu)
         modelMenu.add_command(label = "Switch Topic Model")
 
     #Create all the tabs for the NLP tool
     def createTabs(self):
-        self.tabControl = ttk.Notebook(self.root)
-        self.sentenceTab = ttk.Frame(self.tabControl)
-        self.userTab = ttk.Frame(self.tabControl)
-        self.topicTab = ttk.Frame(self.tabControl)
-        # self.grammarTab = ttk.Frame(self.tabControl)
-        self.baryTab = ttk.Frame(self.tabControl)
-        self.influenceTab = ttk.Frame(self.tabControl)
-        self.userMetricsTab = ttk.Frame(self.tabControl)
-        self.tabControl.add(self.sentenceTab, text = "Statement Analysis")
-        self.tabControl.add(self.userTab, text = "User Analysis")
-        self.tabControl.add(self.topicTab, text = "Topic Extraction")
-        # self.tabControl.add(self.grammarTab, text = "Grammatical Analysis")
-        self.tabControl.add(self.baryTab, text = "Barycentric Allocation")
-        self.tabControl.add(self.influenceTab, text = "Social Influence")
-        self.tabControl.add(self.userMetricsTab, text = "User Metrics")
-        self.tabControl.pack(expand = 1, fill = "both")
+        tabControl = ttk.Notebook(self.root)
+        self.sentenceTab = ttk.Frame(tabControl)
+        self.userTab = ttk.Frame(tabControl)
+        self.topicTab = ttk.Frame(tabControl)
+        # self.grammarTab = ttk.Frame(tabControl)
+        self.baryTab = ttk.Frame(tabControl)
+        self.influenceTab = ttk.Frame(tabControl)
+        self.userMetricsTab = ttk.Frame(tabControl)
+        tabControl.add(self.sentenceTab, text = "Statement Analysis")
+        tabControl.add(self.userTab, text = "User Analysis")
+        tabControl.add(self.topicTab, text = "Topic Extraction")
+        # tabControl.add(self.grammarTab, text = "Grammatical Analysis")
+        tabControl.add(self.baryTab, text = "Barycentric Allocation")
+        tabControl.add(self.influenceTab, text = "Social Influence")
+        tabControl.add(self.userMetricsTab, text = "User Metrics")
+        tabControl.pack(expand = 1, fill = "both")
+
+    #Create all the label frames required for all the tabs
+    def createFrames(self):
+        self.sentenceFrame = ttk.LabelFrame(self.sentenceTab, text = "Sentence Data")
+
+        self.sentenceFrame.grid(row = 0, column = 0, rowspan = 3, padx = 5, pady = 5, ipadx = 5, ipady = 5, sticky="nw")
+
+    #Create all the listboxes required for all the tabs
+    def createListboxes(self):
+        scrollbar = Scrollbar(self.sentenceFrame)
+        h_scrollbar = Scrollbar(self.sentenceFrame, orient = HORIZONTAL)
+        self.discussion_listbox = Listbox(self.sentenceFrame, width = 60, height = 18, yscrollcommand = scrollbar.set, xscrollcommand = h_scrollbar.set, selectmode = EXTENDED, exportselection=False)
+        scrollbar.pack(side = "right", fill = 'y')
+        self.discussion_listbox.pack()
+        h_scrollbar.pack(fill = 'x')
+        scrollbar.config(command = self.discussion_listbox.yview)
+        h_scrollbar.config(command = self.discussion_listbox.xview)
+        # self.discussion_listbox.bind("<<ListboxSelect>>", callbackSentences)
